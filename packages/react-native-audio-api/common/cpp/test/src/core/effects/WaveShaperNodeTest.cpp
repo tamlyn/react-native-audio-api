127a972d7472e2a932d5a038838a9772552cd230
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2025-11-12 21:46:04 +0000

    chore: tests draft for wave shaper and resampler

---
+++ packages/react-native-audio-api/common/cpp/test/src/core/effects/WaveShaperNodeTest.cpp
@@ -0,0 +1,45 @@
+#include <audioapi/core/OfflineAudioContext.h>
+#include <audioapi/core/effects/WaveShaperNode.h>
+#include <audioapi/core/utils/worklets/SafeIncludes.h>
+#include <audioapi/utils/AudioArray.h>
+#include <audioapi/utils/AudioBus.h>
+#include <gtest/gtest.h>
+#include <test/src/MockAudioEventHandlerRegistry.h>
+
+using namespace audioapi;
+
+class WaveShaperNodeTest : public ::testing::Test {
+ protected:
+  std::shared_ptr<MockAudioEventHandlerRegistry> eventRegistry;
+  std::unique_ptr<OfflineAudioContext> context;
+  static constexpr int sampleRate = 44100;
+
+  void SetUp() override {
+    eventRegistry = std::make_shared<MockAudioEventHandlerRegistry>();
+    context = std::make_unique<OfflineAudioContext>(
+        2, 5 * sampleRate, sampleRate, eventRegistry, RuntimeRegistry{});
+  }
+};
+
+class TestableWaveShaperNode : public WaveShaperNode {
+ public:
+  explicit TestableStereoPannerNode(BaseAudioContext *context)
+      : StereoPannerNode(context) {}
+
+  std::shared_ptr<AudioBus> processNode(
+      const std::shared_ptr<AudioBus> &processingBus,
+      int framesToProcess) override {
+    return WaveShaperNode::processNode(processingBus, framesToProcess);
+  }
+};
+
+TEST_F(WaveShaperNodeTest, WaveShaperNodeCanBeCreated) {
+  auto waveShaper = context->createWaveShaper();
+  ASSERT_NE(waveShaper, nullptr);
+}
+
+// curve alg test1, test2...
+
+// no curve test
+
+// does none, 2x, 4x return correct number of frames
---
+++ packages/react-native-audio-api/common/cpp/test/src/dsp/ResamplerTest.cpp
@@ -0,0 +1,21 @@
+#include <audioapi/dsp/Resampler.h>
+#include <audioapi/core/utils/worklets/SafeIncludes.h>
+#include <audioapi/utils/AudioArray.h>
+#include <audioapi/utils/AudioBus.h>
+#include <gtest/gtest.h>
+#include <test/src/MockAudioEventHandlerRegistry.h>
+
+using namespace audioapi;
+
+class ResamplerTest : public ::testing::Test {
+ protected:
+  std::shared_ptr<Resampler> resampler_;
+
+  void SetUp() override {
+    resampler_ = std::make_shared<Resampler>()
+  }
+};
+
+// number of output frames test
+
+// quality test??? sin ???
