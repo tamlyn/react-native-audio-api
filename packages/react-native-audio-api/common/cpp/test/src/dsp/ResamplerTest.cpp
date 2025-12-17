763914d61b91c3a593577cdbeff5467400c53a79
maciejmakowski2003 <maciej.makowski2608@gmail.com>
2025-12-17 22:52:52 +0100

    test: refactor

--- packages/react-native-audio-api/common/cpp/test/src/dsp/ResamplerTest.cpp
+++ packages/react-native-audio-api/common/cpp/test/src/dsp/ResamplerTest.cpp
@@ -93,85 +93,7 @@ TEST_F(ResamplerTest, DownSamplerKernelSum) {
   EXPECT_NEAR(sum, 1.0f, 1e-6);
 }
 
-TEST_F(ResamplerTest, UpSamplerProcess) {
-  auto upSampler = std::make_unique<TestableUpSampler>(RENDER_QUANTUM_SIZE, RENDER_QUANTUM_SIZE);
-  auto kernel = upSampler->getKernel();
-
-  auto stateBuffer = std::make_shared<AudioArray>(2 * RENDER_QUANTUM_SIZE);
-  stateBuffer->zero();
-
-  auto inputArray = std::make_shared<AudioArray>(4);
-  (*inputArray)[0] = 1.0f;
-  (*inputArray)[1] = 0.0f;
-  (*inputArray)[2] = -1.0f;
-  (*inputArray)[3] = 1.0f;
-
-  auto outputArray = std::make_shared<AudioArray>(8);
-
-  memcpy(
-      stateBuffer->getData() + KERNEL_SIZE,
-      inputArray->getData(),
-      inputArray->getSize() * sizeof(float));
-
-  upSampler->process(inputArray, outputArray, 4);
-
-  for (size_t i = 0; i < outputArray->getSize(); ++i) {
-    auto idx = RENDER_QUANTUM_SIZE - RENDER_QUANTUM_SIZE / 2 + i / 2;
-
-    if (i % 2 == 0) {
-      EXPECT_FLOAT_EQ(outputArray->getData()[i], stateBuffer->getData()[idx]);
-    } else {
-      float sum = 0.0f;
-
-      for (size_t k = 0; k < kernel->getSize(); ++k) {
-        sum += stateBuffer->getData()[i / 2 + 1 + k] * kernel->getData()[k];
-      }
-
-      EXPECT_FLOAT_EQ(outputArray->getData()[i], sum);
-    }
-  }
-}
-
-TEST_F(ResamplerTest, DownSamplerProcess) {
-  auto downSampler =
-      std::make_unique<TestableDownSampler>(RENDER_QUANTUM_SIZE * 2, RENDER_QUANTUM_SIZE * 2);
-  auto kernel = downSampler->getKernel();
-
-  auto stateBuffer = std::make_shared<AudioArray>(4 * RENDER_QUANTUM_SIZE);
-  stateBuffer->zero();
-
-  auto inputArray = std::make_shared<AudioArray>(8);
-  (*inputArray)[0] = 1.0f;
-  (*inputArray)[1] = 0.0f;
-  (*inputArray)[2] = -1.0f;
-  (*inputArray)[3] = 1.0f;
-  (*inputArray)[4] = 0.5f;
-  (*inputArray)[5] = -0.5f;
-  (*inputArray)[6] = 0.25f;
-  (*inputArray)[7] = -0.25f;
-
-  auto outputArray = std::make_shared<AudioArray>(4);
-
-  memcpy(
-      stateBuffer->getData() + KERNEL_SIZE,
-      inputArray->getData(),
-      inputArray->getSize() * sizeof(float));
-
-  downSampler->process(inputArray, outputArray, 8);
-
-  for (size_t i = 0; i < outputArray->getSize() / 8; ++i) {
-    auto idx = RENDER_QUANTUM_SIZE + i * 2;
-    float sum = 0.0f;
-
-    for (size_t k = 0; k < kernel->getSize(); ++k) {
-      sum += stateBuffer->getData()[idx + k] * kernel->getData()[k];
-    }
-
-    EXPECT_FLOAT_EQ(outputArray->getData()[i], sum);
-  }
-}
-
-TEST_F(ResamplerTest, UpDownSamplingProcessThrowsNoErrors) {
+TEST_F(ResamplerTest, UpDownSamplingProcess) {
   auto upSampler = std::make_unique<TestableUpSampler>(RENDER_QUANTUM_SIZE, RENDER_QUANTUM_SIZE);
   auto downSampler =
       std::make_unique<TestableDownSampler>(RENDER_QUANTUM_SIZE * 2, RENDER_QUANTUM_SIZE * 2);
@@ -184,6 +106,12 @@ TEST_F(ResamplerTest, UpDownSamplingProcessThrowsNoErrors) {
 
   auto outputArray = std::make_shared<AudioArray>(8);
 
-  EXPECT_NO_THROW(upSampler->process(inputArray, outputArray, 4));
-  EXPECT_NO_THROW(downSampler->process(outputArray, inputArray, 8));
+  int upSamplerOutputFrames;
+  int downSamplerOutputFrames;
+
+  EXPECT_NO_THROW(upSamplerOutputFrames = upSampler->process(inputArray, outputArray, 4));
+  EXPECT_NO_THROW(downSamplerOutputFrames = downSampler->process(outputArray, inputArray, 8));
+
+  EXPECT_EQ(upSamplerOutputFrames, 8);
+  EXPECT_EQ(downSamplerOutputFrames, 4);
 }
